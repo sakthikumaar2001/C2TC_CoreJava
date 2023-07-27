2311a5ffe9eb4f847e4986c70ca930b6a9c4dc7f
91638 <91638@Sakthi>
2023-07-27 12:49:57 +0530

    update_1

---
+++ operators/src/operators/Basic.java
@@ -0,0 +1,19 @@
+package operators;
+
+import java.util.Scanner;
+
+public class Basic {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		int Add;
+		int a = sc.nextInt();
+		int b = sc.nextInt();
+		
+		Add= a+b;
+		
+		System.out.println(Add);
+	}
+
+}
